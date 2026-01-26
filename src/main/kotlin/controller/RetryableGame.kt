d724a1e39cc8891f6fb6861b4a77e14e68363c91
apdkeb <apdkeb@naver.com>
2026-01-26 21:09:21 +0900

    while 조건 안에 게임 진행 횟수 증가를 while 구문 안으로 옮김.

--- src/main/kotlin/controller/RetryableGame.kt
+++ src/main/kotlin/controller/RetryableGame.kt
@@ -9,7 +9,8 @@ import view.ResultView
 class RetryableGame(val wordleGame: WordleGame, val resultView: ResultView, val input: InputView) {
     fun run(answer: GameString) {
         var currentCount = 0
-        while (currentCount++ < Constant.MAX_GAME_TRY_COUNT) {
+        while (currentCount < Constant.MAX_GAME_TRY_COUNT) {
+            currentCount++
             val input = input.getInput()
 
             val gameResult = wordleGame.logic(input, answer)
