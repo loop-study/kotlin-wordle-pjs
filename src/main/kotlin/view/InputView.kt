4e4dc87629e54bedf92eaa610f45cc8cd5fd13da
apdkeb <apdkeb@naver.com>
2026-01-31 19:22:03 +0900

    - 5글자 안내 문구 추가.

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -1,10 +1,12 @@
 package view
 
+import domain.Constant
 import domain.GameString
 
 class InputView {
 
     fun getInput(): GameString {
+        println("${Constant.WORD_LENGTH}글자 단어를 입력해 주세요");
         while(true) {
             val inputWord = readln().trim()
             try {
