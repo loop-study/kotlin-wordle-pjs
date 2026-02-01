befd6b27085a0c1aa5d3e34e453bb5b53e0dbfd1
apdkeb <apdkeb@naver.com>
2026-02-01 15:09:02 +0900

    - 불필요한 외부 노출 제거

--- src/main/kotlin/controller/RetryableGame.kt
+++ src/main/kotlin/controller/RetryableGame.kt
@@ -6,7 +6,11 @@ import domain.WordleGame
 import view.InputView
 import view.ResultView
 
-class RetryableGame(val wordleGame: WordleGame, val resultView: ResultView, val inputView: InputView) {
+class RetryableGame(
+    private val wordleGame: WordleGame,
+    private val resultView: ResultView,
+    private val inputView: InputView
+) {
     fun run(answer: GameString) {
         repeat (Constant.MAX_GAME_TRY_COUNT) {
             val inputWord = inputView.getInput()
