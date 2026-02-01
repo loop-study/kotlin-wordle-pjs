13dfbbd4922d2e85deb21e0e4ed73a94d6efec30
apdkeb <apdkeb@naver.com>
2026-02-01 15:08:17 +0900

    - NPE 유발코드 제거, 엘비스 연산자로 null 대응

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -8,7 +8,7 @@ class InputView {
     fun getInput(): GameString {
         println("${Constant.WORD_LENGTH}글자 단어를 입력해 주세요");
         while(true) {
-            val inputWord = readln().trim()
+            val inputWord = readlnOrNull()?.trim() ?: ""
             try {
                 return GameString(inputWord)
             } catch (exception: Exception) {
